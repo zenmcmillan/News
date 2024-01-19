435b6ed6547d5759b317f71419b88b73cc0fe64b
Zen McMillan <zenmcmillan@gmail.com>
2024-01-19 14:22:11 -0600

    Style Title header and search bar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1 className="news-header">News</h1>
+      <h2 className="articles-title">Articles</h2>
+      <div className='search-box-container'>
+        <input className="search-box" />
+      </div>
+    </main>
+  );
+}
+
+export default App;
