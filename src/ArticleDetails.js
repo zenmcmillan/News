2aab20c0eb158ea00438fdf8c4ef461bd66ce59c
Zen McMillan <zenmcmillan@gmail.com>
2024-01-19 20:12:08 -0600

    Make a function to convert date into proper format

--- src/ArticleDetails.js
+++ src/ArticleDetails.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import './ArticleDetails.css'
 
 export default function ArticleDetails({articles}) {
   const id = useParams().id
 
   let theArticle = articles.find((element) => element.id === parseInt(id))
 
-  console.log("ARTICLE 1", theArticle);
-   
-
   if (theArticle) {
+    
+    const isoString = theArticle.publishedAt;
+    const dateObject = new Date(isoString)
+
+    const format = {month: 'short', day: 'numeric', year: 'numeric'}
+    const formattedDate = dateObject.toLocaleString('en-US', format)
+    console.log(formattedDate)
+   
     return (
       <div className='article-info-container'>
         <h3>{theArticle.title}</h3>
+        <img src={theArticle.urlToImage} />
         <p>{theArticle.description}</p>
-
+        <p>{formattedDate}</p>
       </div>
     );
   }
