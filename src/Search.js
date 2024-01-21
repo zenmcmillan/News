320cd6a72e9c7df311f4591dcd1f11402c5849be
Zen McMillan <zenmcmillan@gmail.com>
2024-01-21 12:13:47 -0600

    Create basic search functionality

--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,31 @@
 import React from "react";
 import "./Search.css";
+import { useState } from  "react";
 
-export default function Search() {
-  // const onHomePage = window.location.pathname === '/';
+export default function Search({articles, setArticles}) {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  }
+
+  const searchArticle = (value) => {
+    console.log("VALUE", value)
+  let filtered = articles.filter(element => element.title.includes(value))
+   setArticles(filtered)
+}
+
+const onSearch = (value) => {
+  console.log("SEARCH", value);
+  searchArticle(value);
+};
+  
   return (
-
         <div>
           <h2 className="articles-title">Articles</h2>
           <div className="search-box-container">
-            <input className="search-box" />
-            <button>Search</button>
+            <input className="search-box" type="text" value={value} onChange={onChange}/>
+            <button onClick={() => onSearch(value)}>Search</button>
           </div>
         </div>
         )
