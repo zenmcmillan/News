4b9401f74841b84e4da215fe5ec1aa842e9f9174
Zen McMillan <zenmcmillan@gmail.com>
2024-01-21 14:34:57 -0600

    Home page is responsive to the various dimensions

--- src/Card.js
+++ src/Card.js
@@ -5,11 +5,11 @@ import './Card.css'
 export default function Card({title, image, description, date, id}) {
   return (
     <div className="card">
-      <h3>
+      <h3 className="card-info">
         <Link to={`${id}`}>{title}</Link>
       </h3>
       <img src={image} />
-      <p>{description}</p>
+      <p className="card-info">{description}</p>
       <p>{date}</p>
     </div>
   );
